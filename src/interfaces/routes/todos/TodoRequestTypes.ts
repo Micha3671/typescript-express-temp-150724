49252d39e303ae9c634059969448d2ccf28af98f
Micha <michael.rigas@t-online.de>
2024-07-15 13:35:35 +0200

    prettier commit

--- src/interfaces/routes/todos/TodoRequestTypes.ts
+++ src/interfaces/routes/todos/TodoRequestTypes.ts
@@ -2,3 +2,22 @@ export interface IMarkTodoBody {
   todoId: number;
   newIsDone: boolean;
 }
+
+export interface IUpdateTodoBody {
+  todoId: number;
+  newTask: string;
+  newIsDone: boolean;
+  newDueDate: Date;
+}
+
+export interface ICreateTodoBody {
+  todoId: number;
+  newTask: string;
+  newIsDone: boolean;
+  newDueDate: Date;
+  newUserId: number;
+}
+
+export interface IDeleteTodoBody {
+  todoId: number;
+}
